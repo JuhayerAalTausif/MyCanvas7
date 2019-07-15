281ff53b261a96850a6d155afa1c69fa41e4fa2c
Juhayer Al Tausif <tausifwarior@gmail.com>
2019-07-15 17:39:30 +0600

    Modified canvas.js

--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-var canvas = document.querySelector('canvas');
+var canvas = document.getElementById('canvas');
 var c = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
